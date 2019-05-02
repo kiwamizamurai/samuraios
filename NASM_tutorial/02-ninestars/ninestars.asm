30db9cf4382ca936c34647095f7ea5032444d017
kiwamizamurai <24860100+kiwamizamurai@users.noreply.github.com>
2019-05-02 10:31:03 +0900

    [MOD] some comments

--- NASM_tutorial/02-ninestars/ninestars.asm
+++ NASM_tutorial/02-ninestars/ninestars.asm
@@ -11,6 +11,7 @@ _main:           ; tell linker entry point
     mov rsi, s2  ; message to write
     mov rdi, 1   ; file descriptor (stdout)
     mov rax, 0x2000004   ; system call number (sys_write)
+    ; Mac OS X 64bitでは、syscall番号に0x2000000を足して下さい。（標準出力は4なので0x2000004）
     syscall
     mov rax, 0x2000001   ; system call number (sys_exit)
     syscall
@@ -19,3 +20,6 @@ section  .data
 msg: db 'Displaying 9 stars',0xa ; a message
 len: equ $ - msg                 ; length of message
 s2: times 9 db '*'
+
+
+; https://dev.classmethod.jp/etc/asm_mov/
